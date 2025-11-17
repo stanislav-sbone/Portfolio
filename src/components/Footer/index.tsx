806bc0fd0626b38db8eb7f0af91fe6a44b866a55
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-11-17 22:42:44 +0300

    refactor(footer): generate social links from array

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,50 +2,44 @@ import { type FC } from 'react';
 import styles from './Footer.module.css';
 
 const Footer: FC = () => {
+  const socials = [
+    {
+      href: 'https://t.me/stasxd',
+      name: 'telegram',
+      image: '/images/telegram.png',
+    },
+    {
+      href: 'https://github.com/stanislav-sbone',
+      name: 'GitHub',
+      image: '/images/github.png',
+    },
+    {
+      href: 'https://www.linkedin.com/in/stanislav-borisenko-091658398/',
+      name: 'linkedin',
+      image: '/images/linkedin.svg',
+    },
+  ];
+
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <p className={styles.text}>© Stanislav Borisenko | 2025</p>
         <div className={styles.socials}>
-          <a
-            href="https://t.me/stasxd"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="telegram"
-            className={styles.link}
-          >
-            <img
-              className={styles.linkImage}
-              src="/images/telegram.png"
-              alt="telegram"
-            />
-          </a>
-          <a
-            href="https://github.com/stanislav-sbone"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="GitHub"
-            className={styles.link}
-          >
-            <img
-              className={styles.linkImage}
-              src="/images/github.png"
-              alt="github"
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/stanislav-borisenko-091658398/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="linkedin"
-            className={styles.link}
-          >
-            <img
-              className={styles.linkImage}
-              src="/images/linkedin.svg"
-              alt="linkedin"
-            />
-          </a>
+          {socials.map((social) => (
+            <a
+              href={social.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label={social.name}
+              className={styles.link}
+            >
+              <img
+                src={social.image}
+                alt={social.name}
+                className={styles.linkImage}
+              />
+            </a>
+          ))}
         </div>
       </div>
     </footer>
