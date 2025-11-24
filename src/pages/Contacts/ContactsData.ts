a84e3cdd14d42096a41c463f2bfffc595ba83dba
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-11-24 16:16:14 +0300

    refactor(contacts): moved contacts to a separate file

---
+++ src/pages/Contacts/ContactsData.ts
@@ -0,0 +1,26 @@
+export const contacts = [
+  {
+    name: 'Email',
+    href: 'stas.borisenko91@gmail.com',
+    value: 'stas.borisenko91@gmail.com',
+    image: '/images/gmail.png',
+  },
+  {
+    name: 'Telegram',
+    href: 'https://t.me/stasxd',
+    value: '@stasxd',
+    image: '/images/telegram.png',
+  },
+  {
+    name: 'GitHub',
+    href: 'https://github.com/stanislav-sbone',
+    value: 'github.com/stanislav-sbone',
+    image: '/images/github.png',
+  },
+  {
+    name: 'Linkedin',
+    href: 'https://www.linkedin.com/in/stanislav-borisenko-091658398/',
+    value: 'stanislav-borisenko',
+    image: '/images/linkedin.svg',
+  },
+];
--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -2,35 +2,9 @@ import type { FC } from 'react';
 import styles from './Contacts.module.css';
 import Contact from './Contact';
 import ContactForm from './ContactForm';
+import { contacts } from './ContactsData';
 
 const Contacts: FC = () => {
-  const contacts = [
-    {
-      name: 'Email',
-      href: 'stas.borisenko91@gmail.com',
-      value: 'stas.borisenko91@gmail.com',
-      image: '/images/gmail.png',
-    },
-    {
-      name: 'Telegram',
-      href: 'https://t.me/stasxd',
-      value: '@stasxd',
-      image: '/images/telegram.png',
-    },
-    {
-      name: 'GitHub',
-      href: 'https://github.com/stanislav-sbone',
-      value: 'github.com/stanislav-sbone',
-      image: '/images/github.png',
-    },
-    {
-      name: 'Linkedin',
-      href: 'https://www.linkedin.com/in/stanislav-borisenko-091658398/',
-      value: 'stanislav-borisenko',
-      image: '/images/linkedin.svg',
-    },
-  ];
-
   return (
     <section className={styles.contacts}>
       <div className={styles.container}>
