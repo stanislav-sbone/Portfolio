8aecf222ff1e99b68c97e5b52dfcc1c92979129f
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-11-24 16:36:14 +0300

    feat(header): close burgerMenu on outside touch

--- src/components/Header/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import { useEffect, useRef, type FC } from 'react';
 import { navItems } from './headerData';
 import { Link } from 'react-router';
 import { useIsActive } from '../../hooks/useIsActive';
@@ -11,8 +11,23 @@ interface IProps {
 
 const BurgerMenu: FC<IProps> = ({ setIsOpen }) => {
   const isActive = useIsActive();
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const closeOnOuterTouch = (event: TouchEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('touchstart', closeOnOuterTouch);
+    return () => {
+      document.removeEventListener('touchstart', closeOnOuterTouch);
+    };
+  }, [setIsOpen]);
+
   return (
-    <div className={styles.burgerMenu}>
+    <div ref={menuRef} className={styles.burgerMenu}>
       {navItems.map((item) => (
         <Link
           key={item.path}
