807e594f9f4b7c6da126387495fdb4e87af5df75
Станислав Борисенко <stas.borisenko91@gmail.com>
2025-12-05 00:17:51 +0300

    feat(about): replace text list with accordion

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,18 +1,38 @@
-import type { FC } from 'react';
+import { type FC, useState } from 'react';
+import { accordionData } from './AccordionData';
+import AccordionItem from './Accordion';
 import styles from './About.module.css';
-import { aboutParagraphs } from './AboutData';
 
 const About: FC = () => {
+  const [openedItems, setOpenedItems] = useState<Set<string>>(new Set());
+
+  const toggleItem = (key: string) => {
+    setOpenedItems((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(key)) {
+        newSet.delete(key);
+      } else {
+        newSet.add(key);
+      }
+      return newSet;
+    });
+  };
+
   return (
     <section className={styles.about}>
       <div className={styles.container}>
         <h1 className={styles.title}>О себе</h1>
         <div className={styles.content}>
-          <div className={styles.textBlock}>
-            {aboutParagraphs.map((p) => (
-              <p key={p.key} className={styles.text}>
-                {p.text}
-              </p>
+          <div className={styles.accordion}>
+            {accordionData.map((item) => (
+              <AccordionItem
+                key={item.key}
+                id={item.key}
+                title={item.title}
+                text={item.text}
+                isOpened={openedItems.has(item.key)}
+                onToggle={toggleItem}
+              />
             ))}
           </div>
         </div>
